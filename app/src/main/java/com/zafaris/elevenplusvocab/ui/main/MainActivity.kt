d092a43d1058ca2e50d527c4025c3bbc8fcd8678
zafaris <zafaris.dev@gmail.com>
2020-12-21 01:04:09 +0000

    Updated Splash screen

--- app/src/main/java/com/zafaris/elevenplusvocab/ui/main/MainActivity.kt
+++ app/src/main/java/com/zafaris/elevenplusvocab/ui/main/MainActivity.kt
@@ -48,6 +48,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var db: WordBankDbAccess
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
