6a479806b2abe7677a065e287e020947847b2197
zafaris <zafaris.dev@gmail.com>
2020-12-22 17:01:41 +0000

    Renamed Settings Activity layout

--- app/src/main/java/com/zafaris/elevenplusvocab/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/zafaris/elevenplusvocab/ui/settings/SettingsActivity.kt
@@ -10,7 +10,7 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_settings)
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.settings, SettingsFragment())
