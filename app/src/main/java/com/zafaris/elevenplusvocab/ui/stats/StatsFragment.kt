040b35576e8280db3307e4a29488cf2a91f926d8
zafaris <zafaris.dev@gmail.com>
2020-12-29 16:57:54 +0000

    Added placeholder fragments
    - Stats
    - Guide

---
+++ app/src/main/java/com/zafaris/elevenplusvocab/ui/stats/StatsFragment.kt
@@ -0,0 +1,30 @@
+package com.zafaris.elevenplusvocab.ui.stats
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.zafaris.elevenplusvocab.databinding.FragmentStatsBinding
+
+class StatsFragment : Fragment() {
+    private var _binding: FragmentStatsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentStatsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //TODO: Add stuff here
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
